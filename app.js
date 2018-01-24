2e32072cee8c8c7201f0fb24752a23512c923c78
Fernando Renteria <rentfern@gmail.com>
2018-01-24 10:20:14 -0600

    Cleaned up output of JSON object

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ request({
     url: "https://maps.googleapis.com/maps/api/geocode/json?address=3301+Ward+Robert+Place+El+Paso",
     json: true
 },(error, response, body) =>{
-    console.log(body);
+    // last integer is # of spaces
+    console.log(JSON.stringify(body, undefined, 2));
 });
