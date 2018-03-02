ab0eba922bd32f65eb751bd12c7854d3b45ffbbd
Fernando Renteria <rentfern@gmail.com>
2018-03-02 09:17:02 -0600

    Updated app to allow input from users to enter location and receive weather data from API

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const yargs = require("yargs");
 const geocode = require("./geocode/geocode.js");
+const weather = require("./weather/weather.js");
 const argv = yargs
     .options({
         address: {
@@ -17,18 +18,14 @@ geocode.geocodeAddress(argv.a,(errorMessage, results) =>{
     if(errorMessage){
         console.log(errorMessage);
     }else{
-        console.log(JSON.stringify(results, undefined, 2));
-    }
-});
+        console.log(results.address);
 
-const request = require("request");
-request({
-    url: "https://api.darksky.net/forecast/bb36cfc3ba05344a98c6bbf40d4e0a80/37.8267,-122.4233",
-    json: true
-}, (error, response, body) =>{
-    if(!error && response.statusCode == 200){
-        console.log(body.currently.temperature);
-    }else{
-        console.log("Unable to fetch weather");
+        weather.getWeather(results.latitude, results.longitude, (errorMessage, weatherResults) => {
+            if(errorMessage){
+                console.log(errorMessage);
+            }else{
+                console.log(`It's currently ${weatherResults.temperature}. It feels like ${weatherResults.apparentTemperature}.`);
+            }
+        });
     }
 });
